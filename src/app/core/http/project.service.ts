aee61497c1facdddc4ba99d10252abecaab9468f
Yeison Garzón T <toxiicsa@hotmail.com>
2021-02-09 18:47:08 -0500

    ADD: project service

---
+++ src/app/core/http/project.service.ts
@@ -0,0 +1,19 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Project } from '../models/project.model';
+import { environment } from 'src/environments/environment';
+@Injectable({
+  providedIn: 'root'
+})
+export class ProjectService {
+  private baseUrl: string = environment.BASE_API_URL;
+  constructor(private readonly _http: HttpClient) { }
+
+  public getProjects(){
+    return this._http.get<Project>(this.baseUrl + "/project");
+  }
+
+   public getProject(projectId: string){
+    return this._http.get<Project>(this.baseUrl + "/project/"+ projectId);
+  } 
+}
