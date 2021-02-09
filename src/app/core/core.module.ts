cfed7b80f243f30be103ea8e928cfe1039687040
Yeison Garzón T <toxiicsa@hotmail.com>
2021-02-09 18:47:19 -0500

    ADD: core module

---
+++ src/app/core/core.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FooterComponent } from './layout/footer/footer.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+@NgModule({
+  declarations: [FooterComponent],
+  imports: [
+    CommonModule,
+    FontAwesomeModule
+  ],
+  exports: [FooterComponent]
+})
+export class CoreModule { }
