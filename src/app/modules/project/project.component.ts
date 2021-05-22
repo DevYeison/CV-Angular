108608e20898c710cedfe6963d65e381fd5b6dcf
Yeison Garzon T <garzontoroyeison@gmail.com>
2021-05-22 03:48:08 -0500

    ADD: ligthbox image

--- src/app/modules/project/project.component.ts
+++ src/app/modules/project/project.component.ts
@@ -2,7 +2,7 @@ import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Project } from 'src/app/models/project.model';
 import { ProjectService } from '../home/http/project.service';
-
+import { Lightbox, LightboxConfig } from 'ngx-lightbox';
 @Component({
   selector: 'app-project',
   templateUrl: './project.component.html',
@@ -16,20 +16,26 @@ export class ProjectComponent implements OnInit {
   public error: string = "";
   @ViewChild('alert') private alertElement: ElementRef;
 
-  constructor(private _activatedRoute: ActivatedRoute, private _projectService: ProjectService) {
-    if(this.deviceWidth <= 320 ){
+  private _album: Array<{ src: string, thumb: string }> = [];
+
+  constructor(
+    private _activatedRoute: ActivatedRoute,
+    private _projectService: ProjectService, 
+    private _ligthbox: Lightbox,
+    private _ligthboxConfig: LightboxConfig) {
+    if (this.deviceWidth <= 320) {
       this.deviceWidth = 320;
-    }else if(this.deviceWidth >320 && this.deviceWidth <= 375){
+    } else if (this.deviceWidth > 320 && this.deviceWidth <= 375) {
       this.deviceWidth = 375;
-    }else if(this.deviceWidth >375 && this.deviceWidth <= 425){
+    } else if (this.deviceWidth > 375 && this.deviceWidth <= 425) {
       this.deviceWidth = 425;
-    }else if(this.deviceWidth >425 && this.deviceWidth <= 768){
+    } else if (this.deviceWidth > 425 && this.deviceWidth <= 768) {
       this.deviceWidth = 768;
-    }else if(this.deviceWidth >768 && this.deviceWidth <= 1024){
+    } else if (this.deviceWidth > 768 && this.deviceWidth <= 1024) {
       this.deviceWidth = 1024;
-    }else if(this.deviceWidth >1024 && this.deviceWidth <= 1440){
+    } else if (this.deviceWidth > 1024 && this.deviceWidth <= 1440) {
       this.deviceWidth = 1440;
-    }else if(this.deviceWidth >1440 && this.deviceWidth <= 2560){
+    } else if (this.deviceWidth > 1440 && this.deviceWidth <= 2560) {
       this.deviceWidth = 2560;
     }
     this.project = {
@@ -40,26 +46,44 @@ export class ProjectComponent implements OnInit {
       technologies: ["loading..."],
       imgs: ['https://zonegis.es/wp-content/uploads/2020/05/Loading.jpg']
     }
+  
+    _ligthboxConfig.disableScrolling = true;
+    _ligthboxConfig.centerVertically = true;
+    _ligthboxConfig.wrapAround = true;
+    _ligthboxConfig.resizeDuration = 0.3;
   }
 
   ngOnInit(): void {
-    this._activatedRoute.params.subscribe(params =>{
+    this._activatedRoute.params.subscribe(params => {
       const id: string = params['id'];
       this._projectService.getProject(id, this.deviceWidth)
-      .subscribe(
-        (project: Project)=>{
-        this.loading = true;
-        this.project = project;
-      },
-      (error) =>{
-        this.loading = false;
-        this.error = error.message;
-      }
-      );
+        .subscribe(
+          (project: Project) => {
+            this.loading = true;
+            this.project = project;
+            project.imgs.forEach((img) => {
+              this._album.push({
+                src: img,
+                thumb: img
+              });
+            })
+          },
+          (error) => {
+            this.loading = false;
+            this.error = error.message;
+          }
+        );
     });
   }
 
-  closeAlert(){
+  open(index: number): void {
+    this._ligthbox.open(this._album, index);
+  }
+
+  close(): void {
+    this._ligthbox.close();
+  }
+  closeAlert() {
     this.alertElement.nativeElement.classList.remove("show");
   }
 }
--- src/app/modules/project/project.module.ts
+++ src/app/modules/project/project.module.ts
@@ -6,13 +6,15 @@ import { ProjectComponent } from './project.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
+import { LightboxModule } from 'ngx-lightbox';
 
 @NgModule({
   declarations: [ProjectComponent, FooterComponent],
   imports: [
     CommonModule,
     ProjectRoutingModule,
-    FontAwesomeModule
+    FontAwesomeModule,
+    LightboxModule
   ]
 })
 export class ProjectModule { }
